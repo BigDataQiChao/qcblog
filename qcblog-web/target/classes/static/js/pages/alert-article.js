629268b337da321dfdecd8db5e9b91174f93b9b0
bigdataqichao <1416537683@qq.com>
2020-04-03 09:15:36 +0800

    更新目录

---
+++ qcblog-web/target/classes/static/js/pages/alert-article.js
@@ -0,0 +1,9 @@
+window.alert =alert;
+function alert(e){
+    $("body").append('<div id="msg"><div id="msg_top">温馨提醒<span class="msg_close">×</span></div><div id="msg_cont">'+e+'</div><button class="msg_close" id="msg_clear">确定</button></div>');
+    $(".msg_close").click(function (){
+        $("#msg").remove();
+      /*  window.location.reload();
+      $("#content_table").load(location.href+" #content_table");*/
+    });
+}
